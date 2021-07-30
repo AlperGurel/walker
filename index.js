b95bdd0077e36f0ec500c6a807704172903cf349
Alper <itiswhyilive@gmail.com>
2021-07-31 00:46:58 +0300

    Add controls for speed and acceleration

--- index.js
+++ index.js
@@ -1,114 +1,122 @@
+//CONTROLS
+var maxSpeed = 10;
+var accelerationFactor = 1;
+
+var speedInput = document.getElementById("speed");
+speedInput.value = maxSpeed;
+
+var accelerationInput = document.getElementById("acceleration");
+accelerationInput.value = accelerationFactor;
+
+function updateSpeed(value) {
+  maxSpeed = value;
+}
+
+function updateAcceleration(value) {
+  accelerationFactor = value;
+}
 //Get below info from window
 width = 1920;
 height = 1080;
 
-class Walker{
-    constructor() {
-        this.distributionx = gaussian(0, 2);
-        this.distributiony = gaussian(0, 2);
-        this.position = createVector(width / 2, height / 2);
-        this.clickedPosition = null;
+class Walker {
+  constructor() {
+    this.distributionx = gaussian(0, 2);
+    this.distributiony = gaussian(0, 2);
+    this.position = createVector(width / 2, height / 2);
+    this.clickedPosition = null;
+  }
+
+  display() {
+    stroke(0);
+    point(this.position.x, this.position.y);
+  }
+
+  step() {
+    if (this.clickedPosition) {
+      //let difference = this.clickedPosition.sub(this.position);
+      let difference = p5.Vector.sub(this.clickedPosition, this.position);
+      difference.normalize();
+      this.distributionx = gaussian(difference.x, 0.5);
+      this.distributiony = gaussian(difference.y, 0.5);
     }
+    let stepx = this.distributionx.ppf(Math.random());
+    let stepy = this.distributiony.ppf(Math.random());
+    let velocity = createVector(stepx, stepy);
+    velocity.normalize();
+    velocity = velocity.mult(2);
+    this.position.add(velocity);
+  }
 
-
-
-    display(){
-        stroke(0);
-        point(this.position.x, this.position.y)
-    }
-
-
-    step() {
-        if(this.clickedPosition){
-            //let difference = this.clickedPosition.sub(this.position);
-            let difference = p5.Vector.sub(this.clickedPosition, this.position);
-            difference.normalize();
-            this.distributionx = gaussian(difference.x, 0.5);
-            this.distributiony = gaussian(difference.y, 0.5);
-        }
-        let stepx = this.distributionx.ppf(Math.random());
-        let stepy = this.distributiony.ppf(Math.random());
-        let velocity = createVector(stepx, stepy);
-        velocity.normalize();
-        velocity = velocity.mult(2);
-        this.position.add(velocity);
-    }
-
-    changeVelocity(){
-        this.clickedPosition = createVector(mouseX, mouseY);
-        console.log(this.clickedPosition.x, this.clickedPosition.y);
-
-    }
-
-
-
-
+  changeVelocity() {
+    this.clickedPosition = createVector(mouseX, mouseY);
+    console.log(this.clickedPosition.x, this.clickedPosition.y);
+  }
 }
 
-class Car{
-    constructor(){
-        this.location = createVector(100, height/2);
-        this.velocity = createVector(0, 0);
-        this.acceleration = createVector(0,0);
-        this.accActive = false;
-        this.decActive = false;
+class Car {
+  constructor() {
+    this.location = createVector(100, height / 2);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+    this.accActive = false;
+    this.decActive = false;
+  }
 
+  display() {
+    stroke(0);
+    background(255, 255, 255);
+    fill(175);
+    ellipse(this.location.x, this.location.y, 16, 16);
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.location.add(this.velocity);
+    if (this.accActive) {
+      this.acceleration.x += 0.001;
+      this.acceleration.x = constrain(this.acceleration.x, 0, 10);
     }
-
-    display(){
-        stroke(0);
-        background(255,255,255);
-        fill(175);
-        ellipse(this.location.x, this.location.y, 16, 16);
+    if (this.decActive) {
+      if (this.velocity.x < 0) {
+        this.acceleration.x = 0;
+      } else {
+        this.acceleration.x -= 0.005;
+      }
     }
-
-    update(){
-        this.velocity.add(this.acceleration);
-        this.location.add(this.velocity);
-        if(this.accActive){
-
-            this.acceleration.x += 0.001;
-            this.acceleration.x = constrain(this.acceleration.x, 0, 10 );
-        }
-        if(this.decActive){
-            if(this.velocity.x < 0){
-                this.acceleration.x = 0;
-            }
-            else{
-                this.acceleration.x -= 0.005;
-
-            }
-        }
-    }
+  }
 }
 
-class Mover{
-    constructor(){
-        this.location = createVector(width/2, height/2);
-        this.velocity = createVector(0, 0);
-        this.acceleration = createVector(0,0);
-    }
+class Mover {
+  constructor() {
+    this.location = createVector(width / 2, height / 2);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+  }
 
-    display(){
-        stroke(0);
-        background(255,255,255);
-        fill(175);
-        ellipse(this.location.x, this.location.y, 50, 50);
-    }
+  display() {
+    stroke(255, 225, 0);
+    background(24, 38, 53);
+    fill(255, 225, 0);
+    ellipse(this.location.x, this.location.y, 50, 50);
+  }
 
-    update(){
-        let mousePosition = createVector(mouseX, mouseY);
-        let directionVector = p5.Vector.sub(mousePosition, this.location);
-        let distance = directionVector.mag();
-        let accelerationMultiplier = distance/50;
-        directionVector.normalize();
-        directionVector.mult(accelerationMultiplier);
-        this.acceleration = directionVector;
+  update() {
+    let mousePosition = createVector(mouseX, mouseY);
+    let directionVector = p5.Vector.sub(mousePosition, this.location);
+    let distance = directionVector.mag();
+    let accelerationMultiplier = distance / accelerationFactor;
+    directionVector.normalize();
+    directionVector.mult(accelerationMultiplier);
+    this.acceleration = directionVector;
+    let friction = p5.Vector.mult(this.acceleration, 0.1);
+    // this.acceleration.set(this.acceleration.x - 10, this.acceleration.y - 10);
+    this.acceleration.sub(friction);
 
-        this.velocity.add(this.acceleration);
-        this.velocity.limit(20);
-        this.location.add(this.velocity);
-    }
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(maxSpeed);
+    this.location.add(this.velocity);
+  }
 }
 
 let w;
@@ -116,43 +124,19 @@ let c;
 let m;
 let canvasPadding = 30;
 
-function setup(){
-    createCanvas(windowWidth - canvasPadding, windowHeight - canvasPadding);
-    //w= new Walker();
-    //c = new Car();
-    m = new Mover();
+function setup() {
+  var canvas = createCanvas(1200, 350);
+  canvas.parent("p5-container");
+  //w= new Walker();
+  //c = new Car();
+  m = new Mover();
 }
 
-
-function draw(){
-     // w.step();
-     // w.display();
-    //c.update();
-    //c.display();
-    m.update();
-    m.display();
-
+function draw() {
+  // w.step();
+  // w.display();
+  //c.update();
+  //c.display();
+  m.update();
+  m.display();
 }
-
-function mouseClicked(){
-    // w.changeVelocity();
-
-}
-
-// function keyPressed(){
-//     if(keyCode === LEFT_ARROW){
-//         c.decActive = true;
-//     }
-//     if(keyCode === RIGHT_ARROW){
-//         c.accActive = true;
-//     }
-// }
-//
-// function keyReleased(){
-//     if(keyCode === LEFT_ARROW){
-//         c.decActive = false;
-//     }
-//     if(keyCode === RIGHT_ARROW){
-//         c.accActive = false;
-//     }
-// }
